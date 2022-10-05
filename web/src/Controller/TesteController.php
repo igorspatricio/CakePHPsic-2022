50ad11542d755a8fa5b67eaf79120651417d85a7
igor <igorsouzapatricio@gmail.com>
2022-10-05 15:57:33 -0400

    Projeto

---
+++ web/src/Controller/TesteController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+class TesteController extends AppController{
+    
+    public function testehome(){
+        return $this->render("testehome");
+    }
+
+    public function testehome21(){
+        return $this->render("testehome2");
+    }
+
+    public function testehome42(){
+        return $this->render("testehome2");
+    }
+}
+
+
